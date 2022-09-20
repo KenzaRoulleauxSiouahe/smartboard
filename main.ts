e5aefaa31f99123a50d5dcedbd4a4649c1299689
KenzaRoulleauxSiouahe <113975771+KenzaRoulleauxSiouahe@users.noreply.github.com>
2022-09-20 16:09:14 +0200

    final changes

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("BUS49")
-    basic.showString("" + randint(0, 60) + "MIN")
+    basic.showNumber(randint(0, 60))
+    basic.showString("MIN")
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("BUS36")
-    basic.showString("" + randint(0, 60) + "MIN")
+    basic.showNumber(randint(0, 60))
+    basic.showString("MIN")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("BUS86")
-    basic.showString("" + randint(0, 60) + "MIN")
+    basic.showNumber(randint(0, 60))
+    basic.showString("MIN")
 })
 basic.showString("BUS TIMER")
 basic.clearScreen()
